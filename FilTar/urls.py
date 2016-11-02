a76ff0012cc8954e69f4fb7694f34171efc2e273
Thomas Bradley <thomas.bradley@tgac.ac.uk>
2016-11-02 10:09:47 +0000

    Remove hardcoded url, and add 'filtar' to namespace

--- FilTar/urls.py
+++ FilTar/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'filtar'
+
 urlpatterns = [
 
     # /filtar/
 
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     
     # /filtar/
-    url(r'^(?P<species_id>[0-9]+)/$', views.detail, name="detail")
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail")
     
 ]
