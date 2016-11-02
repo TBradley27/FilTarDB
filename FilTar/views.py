244dcd40360c0c13548306a28398ae30e68b34e7
Thomas Bradley <thomas.bradley@tgac.ac.uk>
2016-11-02 10:11:29 +0000

    Change from function to class-based representation

--- FilTar/views.py
+++ FilTar/views.py
@@ -1,33 +1,57 @@
-# from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
-from FilTar.models import Species
+from django.views import generic
+from .models import Species
 
-def index(request):
-	 all_species = Species.objects.all() 
-	 #template = loader.get_template('FilTar/index.html')
-	 context = {	'all_species' : all_species, }
-	 return render(request, 'filtar/index.html', context)
-# 	html = ()
-# 	for species in all_species:
-# 	     url = '/filtar/' + str(species.id) + '/'
-# 	     html += ('<a href="' + url + '">' + species.common_name + '</a><br>',)
-	
+class IndexView(generic.ListView):
+	template_name = 'filtar/index.html'
 
-def detail(request, species_id):
-# 	try:
-# 		species = Species.objects.get(pk=species_id)
-# 	except Species.DoesNotExist:
-# 		raise Http404("Species does not exist")
-	species = get_object_or_404(Species, pk=species_id)
-	return render(request, 'filtar/detail.html', ({'species': species}) )
-# 	return HttpResponse("<h2>Details for Species id: " + str(species_id) + "</h2>")
+	def get_queryset(selfself):
+		return Species.objects.all()
+
+class DetailView(generic.DetailView):
+	model = Species
+	template_name = 'filtar/detail.html'
 
 
 
-# 	return HttpResponse("<h2>Details for species: " + str(species_id) + "</h2>")
 
 
 
-# Create your views here.
+
+
+
+
+
+# # from django.http import Http404
+#
+# from django.shortcuts import render, get_object_or_404
+# # from django.http import HttpResponse
+# from FilTar.models import Species
+#
+# def index(request):
+# 	 all_species = Species.objects.all()
+# 	 #template = loader.get_template('FilTar/index.html')
+# 	 context = {	'all_species' : all_species, }
+# 	 return render(request, 'filtar/index.html', context)
+# # 	html = ()
+# # 	for species in all_species:
+# # 	     url = '/filtar/' + str(species.id) + '/'
+# # 	     html += ('<a href="' + url + '">' + species.common_name + '</a><br>',)
+#
+#
+# def detail(request, species_id):
+# # 	try:
+# # 		species = Species.objects.get(pk=species_id)
+# # 	except Species.DoesNotExist:
+# # 		raise Http404("Species does not exist")
+# 	species = get_object_or_404(Species, pk=species_id)
+# 	return render(request, 'filtar/detail.html', ({'species': species}) )
+# # 	return HttpResponse("<h2>Details for Species id: " + str(species_id) + "</h2>")
+#
+#
+#
+# # 	return HttpResponse("<h2>Details for species: " + str(species_id) + "</h2>")
+#
+#
+#
+# # Create your views here.
 
