a9333e906ce195d1a38ae4dfa3dc75122ff5868f
TBradley27 <thomas.bradley435@gmail.com>
2019-09-19 20:22:09 +0000

    rename project and the single project app

---
+++ filtardb_app/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+from dal.test.utils import fixtures
+
+from django.db.models.signals import post_migrate
+
+class FiltardbConfig(AppConfig):
+    name = 'filtardb_app'
+
+class TestApp(AppConfig):
+    name = 'select2_many_to_many'
+
+    def ready(self):
+        post_migrate.connect(fixtures, sender=self)
